516c24bedc9d17e6c399c70bf5923a6ff104963a
Artem Skorbezh <artyom.ireday@gmail.com>
2023-05-01 20:00:54 +0300

    feat: add dist to deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Keyboard from './js/VirtualKeyboard';
-import localStorage from './js/LocalStorage';
-import keyboardRows from './js/languages/keyboardRows';
-
-const lang = localStorage.get('lang', 'en');
-
-const keyboard = new Keyboard(keyboardRows);
-keyboard.init(lang);
